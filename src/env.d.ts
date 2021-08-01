724c411fb8b49156b3d9a88fad27d8b6d84b163d
Chris-Robin Ennen <chris@ncteam.de>
2021-08-01 19:28:05 +0200

    add minimal search functionality

---
+++ src/env.d.ts
@@ -0,0 +1,3 @@
+interface ImportMetaEnv {
+	VITE_GENIUS_CLIENT_ACCESS_TOKEN: string;
+}
---
+++ src/routes/search/index.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	let query = '';
+	let songs = [];
+
+	const handleSubmit = async () => {
+		const res = await fetch(`/search/${query}.json`);
+		songs = (await res.json()).songs;
+		console.log(songs);
+	};
+</script>
+
+<h2>Search for artist</h2>
+<form on:submit|preventDefault={handleSubmit}>
+	<input type="text" bind:value={query} />
+	<button type="submit">search</button>
+</form>
+<ul>
+	{#each songs as song}
+		<li>
+			{song.title}
+		</li>
+	{/each}
+</ul>
