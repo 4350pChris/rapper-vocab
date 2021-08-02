139399678ae445bf7cf8dbcf3b1377454d958890
Chris-Robin Ennen <chris@ncteam.de>
2021-08-02 02:49:42 +0200

    add tailwind and some base styles

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,7 @@
+<script>
+  import "../app.postcss";
+</script>
+
+<main class="container mx-auto py-4 flex flex-col items-center">
+  <slot></slot>
+</main>
---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+const config = {
+	mode: "jit",
+	darkMode: "media",
+	purge: [
+		"./src/**/*.{html,js,svelte,ts}",
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
+
+module.exports = config;
