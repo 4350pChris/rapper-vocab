6286bb6927bf0ff6016c02eb0332eadf2e999bd9
Chris-Robin Ennen <chris@ncteam.de>
2021-08-14 14:06:00 +0200

    fix song crawler

--- src/routes/artists/[artist]/songs.ts
+++ src/routes/artists/[artist]/songs.ts
@@ -2,12 +2,13 @@ import { getSongs, putSongs } from '$lib/db';
 import { parseDocument, DomUtils } from 'htmlparser2';
 import type { RequestHandler } from '@sveltejs/kit';
 
-async function getPaginatedSongs(artist: string) {
+async function getPaginatedSongs(artist: string): Promise<Record<string, any>[]> {
 	const baseUrl = `https://api.genius.com/artists/${artist}/songs?sort=popularity`;
+	const existing = (await getSongs(artist)).Items.map((v) => parseInt(v.id.N));
 
 	let page = 1;
-	const chunks = [];
-	while (page !== null && page <= 10) {
+	const songs: Record<string, any>[] = [];
+	while (page !== null && songs.length < 10) {
 		const res = await fetch(baseUrl + '&page=' + page, {
 			headers: {
 				authorization: 'Bearer ' + import.meta.env.VITE_GENIUS_CLIENT_ACCESS_TOKEN
@@ -16,11 +17,11 @@ async function getPaginatedSongs(artist: string) {
 		const json: { songs: Array<Record<string, any>>; next_page: number } = (await res.json())
 			.response;
 		page = json.next_page;
-		// filter out songs that do not have the specified artist as their primary artist
-		chunks.push(json.songs.filter(({ primary_artist }) => primary_artist.id === parseInt(artist)));
+		// filter out songs that are already in the db or do not have the specified artist as their primary artist
+		const filtered = json.songs.filter(({ id, primary_artist }) => !existing.includes(id) && primary_artist.id === parseInt(artist))
+		songs.push(...filtered)
 	}
-
-	return chunks;
+	return songs;
 }
 
 async function crawlLyrics(songUrl: string) {
@@ -42,24 +43,16 @@ export const get: RequestHandler<{ artist: string }> = async ({ params }) => {
 
 export const post: RequestHandler<{ artist: string }> = async ({ params }) => {
 	const { artist } = params;
-	const chunks = [];
 
-	const existing = (await getSongs(artist)).Items.map((v) => parseInt(v.id.N));
 	const songs = await getPaginatedSongs(artist);
-	const filtered = songs.map((chunk) => chunk.filter((song) => !existing.includes(song.id))).filter(chunk => chunk.length > 0);
-	for (const chunk of filtered) {
-		const crawlRequests = chunk.map(async (song) => ({
-			lyrics: await crawlLyrics(song.url),
-			...song
-		}));
-		const lyrics = (await Promise.all(crawlRequests)).filter(({ lyrics }) => !!lyrics);
-		if (lyrics.length) {
-			chunks.push(lyrics);
-		}
-	}
+	const crawlRequests = songs.map(async (song) => ({
+		lyrics: await crawlLyrics(song.url),
+		...song
+	}));
+	const lyrics = (await Promise.all(crawlRequests)).filter(({ lyrics }) => !!lyrics);
 
-	if (chunks.length) {
-		await putSongs(chunks);
+	if (lyrics.length) {
+		await putSongs(lyrics);
 	}
 
 	return { status: 201 };
