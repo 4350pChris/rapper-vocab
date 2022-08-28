fcad8bd703b85e75b4dd37b60ac08b2f266d5a08
Chris-Robin Ennen <chris-robin@ennen.dev>
2022-08-28 22:46:34 +0200

    add basic overview

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,8 +3,11 @@
   import "../app.postcss"
 </script>
 
-<header class="z-50 border-b border-gray-300 dark:border-gray-500 dark:bg-black bg-gray-100 fixed inset-x-0 top-0">
+<header
+  class="z-50 border-b border-gray-300 dark:border-gray-500 dark:bg-black bg-gray-100 fixed inset-x-0 top-0"
+>
   <div class="container h-16 mx-auto flex justify-center items-center">
+    <a class="mr-8 font-bold" href="/">Overview</a>
     <Search />
   </div>
 </header>
